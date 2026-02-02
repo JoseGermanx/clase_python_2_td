6379ba46c84446bb7be6909fa4448f60381b1bcd
Jose G Martinez <jgermanmm@gmail.com>
2026-02-02 19:56:14 -0300

    errrores 01 y 02

---
+++ ERRORES/errores_01.py
@@ -0,0 +1,8 @@
+
+
+
+numero = int(input("Ingresa un número: ")) # "45"
+
+resultado = 10 /numero
+
+print(f"Resultado: {resultado}")
---
+++ ERRORES/errores_02.py
@@ -0,0 +1,22 @@
+
+# ZeroDivisionError
+print("Ejemplo 1")
+# print(10/0)
+
+#ValueError
+print("Ejemplo 2")
+# edad = int("veinticinco")
+
+#IndexError
+print("Ejemplo 3")
+numeros = [1,2,3]
+# print(numeros[5])
+
+#KeyError
+print("Ejemplo 4")
+persona = { "nombre": "Ana"}
+# print(persona["apellido"])
+
+#TypeError
+print("Ejemplo 5")
+resultado = "Total: " + 100
